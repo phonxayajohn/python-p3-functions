f359b7c25002e4d6c54e4eb7053a5002948cc046
John Phonxaya <phonxayajohn@gmail.com>
2023-07-03 17:19:04 -0500

    python functions lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(44,55)
 
 def halve(number):
-    pass
+    print(number/2)
+    return number/2
+
